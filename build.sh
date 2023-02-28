8e7646c9fbc36f234bd040867e38e61b5c36d625
Peter Pan <peter@never.lan>
2023-02-28 13:55:46 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Env setup
+git config --global user.email "peter@never.lan"
+git config --global user.name "Peter Pan"
+
+# Deploy actions
 git checkout prod
 git reset --hard main
 cd www
